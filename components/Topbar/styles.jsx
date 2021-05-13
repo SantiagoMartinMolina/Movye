12fb2ddbf0312f1e1421bc6e70a43bc4cdf1b9cf
Santiago Molina <molina.santiago.martin@gmail.com>
2021-05-13 00:04:59 -0300

    created topbar

--- components/Topbar/styles.jsx
+++ components/Topbar/styles.jsx
@@ -1,5 +1,26 @@
 import styled from 'styled-components';
 
 export const StyledTopbar = styled.div`
-background-color: red;
+    padding: 1.5em 4em; 
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid var(--primary);
+    .user{
+        display: flex;
+        align-items: center;
+        div{
+            margin-right: 0.5em;
+            width: 3em;
+            height: 3em;
+            overflow: hidden;
+            border-radius: 50%;
+            border: 2px solid var(--primary);
+            img{
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+        }
+    }
 `
