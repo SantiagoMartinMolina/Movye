60ff8f1e58770ce5f413fe080adfec5d3e009b18
Santiago Molina <molina.santiago.martin@gmail.com>
2021-05-12 23:35:48 -0300

    initial commit

---
+++ components/Topbar/index.jsx
@@ -0,0 +1,10 @@
+import { StyledTopbar } from './styles';
+import Logo from '../svg/logo';
+const Topbar = (params) => {
+    return (
+        <StyledTopbar>
+            <Logo />
+        </StyledTopbar>
+    )
+}
+export default Topbar;
---
+++ components/Topbar/styles.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledTopbar = styled.div`
+background-color: red;
+`
