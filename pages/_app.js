ea5a89bd89b6032c49d66d5acd883e72c8dc9e0e
Aglowkeys <emilianoalfonso@outlook.com>
2021-06-05 19:10:44 -0300

    Added title to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,27 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 import { MovieContext } from '../context';
 import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+	useEffect(() => {
+		let favorites = JSON.parse(localStorage.getItem('favorites'));
+		let watchlist = JSON.parse(localStorage.getItem('watchlist'));
 
-  useEffect(() => {
+		!favorites && localStorage.setItem('favorites', JSON.stringify([]));
+		!watchlist && localStorage.setItem('watchlist', JSON.stringify([]));
+	}, []);
 
-    let favorites = JSON.parse(localStorage.getItem('favorites'));
-    let watchlist = JSON.parse(localStorage.getItem('watchlist'));
-
-    !favorites && localStorage.setItem('favorites', JSON.stringify([]));
-    !watchlist && localStorage.setItem('watchlist', JSON.stringify([]));
-
-  }, [])
-
-  return <MovieContext><Component {...pageProps} /></MovieContext>
+	return (
+		<>
+			<Head>
+				<title>Movye</title>
+			</Head>
+			<MovieContext>
+				<Component {...pageProps} />
+			</MovieContext>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
